874264ec24603550a3acc26273246d7e013a831b
Thomas PEYRARD <tpeyrard@murex.com>
2017-09-22 12:00:12 +0200

    Extract nested class for range computation

--- src/main/java/com/tpeyrard/ga/tsp/SalesmanAlgorithm.java
+++ src/main/java/com/tpeyrard/ga/tsp/SalesmanAlgorithm.java
@@ -1,64 +1,79 @@
 package com.tpeyrard.ga.tsp;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import com.tpeyrard.ga.FitnessComputation;
 import com.tpeyrard.ga.GeneticAlgorithm;
 import com.tpeyrard.ga.Individual;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class SalesmanAlgorithm extends GeneticAlgorithm {
 
-    private final TourManager tourManager;
+   private final TourManager tourManager;
 
-    public SalesmanAlgorithm(FitnessComputation fitnessCalc, TourManager tourManager) {
-        super(fitnessCalc);
-        this.tourManager = tourManager;
-    }
+   public SalesmanAlgorithm(FitnessComputation fitnessCalc, TourManager tourManager) {
+      super(fitnessCalc);
+      this.tourManager = tourManager;
+   }
 
-    @Override
-    public Individual crossover(Individual firstParent, Individual secondParent, double crossoverRate) {
-        Tour offspring = Tour.tourWithNoCity(tourManager);
+   @Override
+   public Individual crossover(Individual firstParent, Individual secondParent, double crossoverRate) {
+      Tour offspring = Tour.tourWithNoCity(tourManager);
 
-        return orderedCrossover(offspring, firstParent, secondParent);
-    }
+      return orderedCrossover(offspring, firstParent, secondParent);
+   }
 
-    private Individual orderedCrossover(Tour offspring, Individual firstParent, Individual secondParent) {
-        final ThreadLocalRandom localRandom = ThreadLocalRandom.current();
-        final int tempStartPos = localRandom.nextInt(firstParent.genomeSize() + 1);
-        final int tempEndPos = localRandom.nextInt(firstParent.genomeSize() + 1);
+   private Individual orderedCrossover(Tour offspring, Individual firstParent, Individual secondParent) {
+      final Range range = Range.newRange(firstParent.genomeSize());
 
-        final int startPos = Math.min(tempStartPos, tempEndPos);
-        final int endPos = Math.max(tempStartPos, tempEndPos);
+      for (int i = range.start; i < range.end; i++) {
+         offspring.setCity(i, firstParent.gene(i));
+      }
 
-        for (int i = startPos; i < endPos; i++) {
-            offspring.setCity(i, firstParent.gene(i));
-        }
-
-        for (int i = 0; i < secondParent.genomeSize(); i++) {
-            if (!offspring.containsCity(((City) secondParent.gene(i)))) {
-                for (int sparePosition = 0; sparePosition < offspring.genomeSize(); sparePosition++) {
-                    if (offspring.gene(sparePosition) == City.NO_CITY) {
-                        offspring.setCity(sparePosition, secondParent.gene(i));
-                        break;
-                    }
-                }
+      for (int i = 0; i < secondParent.genomeSize(); i++) {
+         if (!offspring.containsCity(((City) secondParent.gene(i)))) {
+            for (int sparePosition = 0; sparePosition < offspring.genomeSize(); sparePosition++) {
+               if (offspring.gene(sparePosition) == City.NO_CITY) {
+                  offspring.setCity(sparePosition, secondParent.gene(i));
+                  break;
+               }
             }
-        }
-        return offspring;
-    }
+         }
+      }
+      return offspring;
+   }
 
-    @Override
-    public void mutate(Individual tour) {
-        swapMutation((Tour) tour);
-    }
+   @Override
+   public void mutate(Individual tour) {
+      swapMutation((Tour) tour);
+   }
 
-    private void swapMutation(Tour offspring) {
-        for (int tourPos1 = 0; tourPos1 < offspring.genomeSize(); tourPos1++) {
-            if (Math.random() < MUTATION_RATE) {
-                int tourPos2 = (int) (offspring.genomeSize() * Math.random());
+   private void swapMutation(Tour offspring) {
+      for (int tourPos1 = 0; tourPos1 < offspring.genomeSize(); tourPos1++) {
+         if (Math.random() < MUTATION_RATE) {
+            int tourPos2 = (int) (offspring.genomeSize() * Math.random());
 
-                offspring.swap(tourPos1, tourPos2);
-            }
-        }
-    }
+            offspring.swap(tourPos1, tourPos2);
+         }
+      }
+   }
+
+   private static final class Range {
+      private int start;
+      private int end;
+
+      private Range(int start, int end) {
+         this.start = start;
+         this.end = end;
+      }
+
+      static Range newRange(int genomeSize) {
+         final ThreadLocalRandom localRandom = ThreadLocalRandom.current();
+         final int tempStartPos = localRandom.nextInt(genomeSize + 1);
+         final int tempEndPos = localRandom.nextInt(genomeSize + 1);
+
+         int startPos = Math.min(tempStartPos, tempEndPos);
+         int endPos = Math.max(tempStartPos, tempEndPos);
+         return new Range(startPos, endPos);
+      }
+   }
 }
